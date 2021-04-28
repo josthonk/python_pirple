6c7f59826c649ee202c5443fdc91f9fd1743d609
josthonk <78995320+josthonk@users.noreply.github.com>
2021-04-28 15:41:30 +0700

    Add files via upload

---
+++ if_statement.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  9 20:54:11 2021
+
+@author: yosua
+"""
+
+# if condition:
+#     Action
+
+click = False #or 0
+
+Like = 0
+
+if click == True:
+    Like = Like + 1
+    click = False
+    
+    
+print(Like)
+
+Temperature = 20
